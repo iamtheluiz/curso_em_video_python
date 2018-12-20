37c6af385c4b9518e20db11268d20d682cd6a996
iamtheluiz <iamtheluiz.dev@gmail.com>
2018-12-20 19:45:05 -0200

    Testes aula 6

---
+++ aula06/soma_float.py
@@ -0,0 +1,6 @@
+n1 = float(input("Digite o valor de n1: "))
+n2 = float(input("Digite o valor de n2: "))
+
+soma = n1 + n2
+
+print("A soma entre {} e {} vale {}".format(n1, n2, soma))
---
+++ aula06/teste_de_tipo.py
@@ -0,0 +1,6 @@
+algo = input("Digite algo: ")
+
+print("É numérico? {}".format(algo.isnumeric()))
+print("É alfabético? {}".format(algo.isalpha()))
+print("É alfanumérico? {}".format(algo.isalnum()))
+print("É decimal? {}".format(algo.isdecimal()))
