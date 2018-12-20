a3b032ab0b44eb6a47888102016eb9bdad5694ae
iamtheluiz <iamtheluiz.dev@gmail.com>
2018-12-20 17:09:52 -0200

    Terminados desafios da aula 04

---
+++ aula04/desafio02.py
@@ -0,0 +1,11 @@
+print("|*****************|")
+print("|    Desafio02    |")
+print("|*****************|")
+print("")
+print("Nascimento")
+
+dia = input("Dia = ")
+mes = input("Mês = ")
+ano = input("Ano = ")
+
+print("Você nasceu dia",dia,"de",mes,"de",ano,". Certo?")
---
+++ aula04/desafio03.py
@@ -0,0 +1,10 @@
+print("|*****************|")
+print("|    Desafio03    |")
+print("|*****************|")
+print("")
+print("Soma")
+
+num1 = int(input("Primeiro Número: "))
+num2 = int(input("Segundo Número:  "))
+
+print("A soma de",num1,"com",num2,"é",num1+num2)
