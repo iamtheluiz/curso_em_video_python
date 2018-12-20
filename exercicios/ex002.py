ed8b83cfd03450e4a96701419bdfa2fc6ea4e3a5
iamtheluiz <iamtheluiz.dev@gmail.com>
2018-12-20 18:55:12 -0200

    Terminado exercicio 2

---
+++ exercicios/ex002.py
@@ -0,0 +1,9 @@
+print("|********************|")
+print("|     Exercício002   |")
+print("|********************|")
+print("")
+print("Ex: Respondendo Usuário!")
+
+nome = input("Qual é seu nome? ")
+
+print("Seja bem-vindo {}!".format(nome))
