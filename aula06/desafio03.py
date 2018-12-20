b1eaa99ad1f712d319834dd29502345937bac8d5
iamtheluiz <iamtheluiz.dev@gmail.com>
2018-12-20 19:53:41 -0200

    Completados desafios da aula 6

---
+++ aula06/desafio03.py
@@ -0,0 +1,11 @@
+print("|*****************|")
+print("|    Desafio03    |")
+print("|*****************|")
+print("")
+print("Soma entre dois números")
+
+n1 = float(input("Digite o valor de n1: "))
+n2 = float(input("Digite o valor de n2: "))
+soma = n1 + n2;
+
+print("A some entre {} e {} é {}".format(n1, n2, soma))
