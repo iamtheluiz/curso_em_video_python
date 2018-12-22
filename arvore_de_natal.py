2128b1c04c2eb48455a450544cf87edb6f8feee8
iamtheluiz <iamtheluiz.dev@gmail.com>
2018-12-22 21:30:39 -0200

    Adicionadas cores na Árvore de Natal

--- arvore_de_natal.py
+++ arvore_de_natal.py
@@ -1,4 +1,4 @@
-print("|***************************|")
+print("\033[31m|***************************|")
 print("|      Arvore de Natal      |")
 print("|  Obs: Tamanho precisa ser |")
 print("| maior que 2               |")
@@ -6,9 +6,9 @@ print("|***************************|")
 tamanho = 1
 
 while tamanho < 3:
-    tamanho = int(input("Qual a altura da sua arvore? "))
+    tamanho = int(input("Qual a altura da sua arvore? \033[m"))
 
-print()
+print("\033[32m")
 contador = 0
 tamanho_final = (tamanho*2) - 1
 
@@ -18,5 +18,5 @@ while contador < tamanho:
     contador += 1
 
 # Coloca o tronco
-print(" "*int(((tamanho_final - 1) / 2)), "|")
+print("\033[m "*int(((tamanho_final - 1) / 2)), "|")
 print(" ", "-"*(tamanho_final-2))
